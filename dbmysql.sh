1e18d37f0fc16d00cf1adf393d7c5a24fadd0985
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-05-02 13:36:34 +0530

    callingotherscript

---
+++ dbmysql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+echo "Please enter DB password:"
+ read mysql_root_password
+
+
+dnf install mysql-server -y &>>$LOGFILE
+
+
+systemctl enable mysqld &>>$LOGFILE
+
+
+systemctl start mysqld &>>$LOGFILE
+
+# Below code is Idempotency nature(run multiple times same output)
+
+ mysql -h dbsql.nirmaladevops.cloud -uroot -p${mysql_root_password} -e 'show databases;'
+
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+   
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
+
+
+
