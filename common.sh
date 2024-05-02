95d82e8093e32a3a684aaad39206151ca5c1f250
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-05-02 17:30:01 +0530

    callingotherscript

--- common.sh
+++ common.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+# set -e
 
- handle_error(){
-    echo "Error occured at line number: $1, error command: $2"
- }
+#  handle_error(){
+#     echo "Error occured at line number: $1, error command: $2"
+#  }
 
- trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+#  trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
 
 USERID=$(id -u)
